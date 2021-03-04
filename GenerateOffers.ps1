9c086527c58f3a7561448a91bef5ccb2add70c72
pbasnal@microsoft.com <pbasnal@microsoft.com>
2021-03-04 13:11:26 +0530

    updating the script to automate build of all offers

--- GenerateOffers.ps1
+++ GenerateOffers.ps1
@@ -1,54 +1,12 @@
-$offername = "eap73-rhel8-byos"
 $offerDropLocation = "offers"
-
 New-Item -ItemType Directory -Force -Path $offerDropLocation
 
-$compress = @{
-    LiteralPath      = "$offername\scripts\", "$offername\createUiDefinition.json", "$offername\mainTemplate.json"
-    CompressionLevel = "Fastest"
-    DestinationPath  = "$offerDropLocation\$offername.zip"
+$offerNames=(Get-ChildItem -Path . -Directory -Force -ErrorAction SilentlyContinue | Select-String eap)
+foreach ($offerName in $offerNames) {
+    $compress = @{
+        LiteralPath      = "$offerName\scripts\", "$offerName\createUiDefinition.json", "$offerName\mainTemplate.json"
+        CompressionLevel = "Fastest"
+        DestinationPath  = "$offerDropLocation\$offerName.zip"
+    }
+    Compress-Archive @compress
 }
-Compress-Archive @compress
-
-
-$offername = "eap73-rhel8-byos-multivm"
-$compress = @{
-    LiteralPath      = "$offername\scripts\", "$offername\createUiDefinition.json", "$offername\mainTemplate.json"
-    CompressionLevel = "Fastest"
-    DestinationPath  = "$offerDropLocation\$offername.zip"
-}
-Compress-Archive @compress
-
-
-$offername = "eap73-rhel8-byos-vmss"
-$compress = @{
-    LiteralPath      = "$offername\scripts\", "$offername\createUiDefinition.json", "$offername\mainTemplate.json"
-    CompressionLevel = "Fastest"
-    DestinationPath  = "$offerDropLocation\$offername.zip"
-}
-Compress-Archive @compress
-
-
-$offername = "eap73-rhel8-payg"
-$compress = @{
-    LiteralPath      = "$offername\scripts\", "$offername\createUiDefinition.json", "$offername\mainTemplate.json"
-    CompressionLevel = "Fastest"
-    DestinationPath  = "$offerDropLocation\$offername.zip"
-}
-Compress-Archive @compress
-
-$offername = "eap73-rhel8-payg-multivm"
-$compress = @{
-    LiteralPath      = "$offername\scripts\", "$offername\createUiDefinition.json", "$offername\mainTemplate.json"
-    CompressionLevel = "Fastest"
-    DestinationPath  = "$offerDropLocation\$offername.zip"
-}
-Compress-Archive @compress
-
-$offername = "eap73-rhel8-payg-vmss"
-$compress = @{
-    LiteralPath      = "$offername\scripts\", "$offername\createUiDefinition.json", "$offername\mainTemplate.json"
-    CompressionLevel = "Fastest"
-    DestinationPath  = "$offerDropLocation\$offername.zip"
-}
-Compress-Archive @compress
