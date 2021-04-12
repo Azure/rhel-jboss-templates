068ded4af991c553f47ecdf59e2800637f8f0569
pbasnal@microsoft.com <pbasnal@microsoft.com>
2021-04-12 13:05:34 +0530

    using systemd to configure and start eap7

--- eap73-rhel8-byos/scripts/jbosseap-setup-redhat.sh
+++ eap73-rhel8-byos/scripts/jbosseap-setup-redhat.sh
@@ -57,16 +57,32 @@ echo "yum groupinstall -y jboss-eap7" | adddate >> /var/log/jbosseap.install.log
 yum groupinstall -y jboss-eap7 >> /var/log/jbosseap.install.log 2>&1
 flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP installation Failed" | adddate >> /var/log/jbosseap.install.log; exit $flag;  fi
 
-echo "Start JBoss-EAP service" | adddate >> /var/log/jbosseap.install.log
-echo "systemctl enable eap7-standalone.service" | adddate >> /var/log/jbosseap.install.log
-systemctl enable eap7-standalone.service | adddate >> /var/log/jbosseap.install.log 2>&1
-echo "echo "WILDFLY_OPTS=-Djboss.bind.address.management=0.0.0.0" >> ${EAP_RPM_CONF_STANDALONE}" | adddate >> /var/log/jbosseap.install.log
-echo 'WILDFLY_OPTS="-Djboss.bind.address.management=0.0.0.0"' >> ${EAP_RPM_CONF_STANDALONE} | adddate >> /var/log/jbosseap.install.log 2>&1
 
-echo "systemctl restart eap7-standalone.service" | adddate >> /var/log/jbosseap.install.log
-systemctl restart eap7-standalone.service | adddate >> /var/log/jbosseap.install.log 2>&1
+echo "Updating standalone-azure-ha.xml" | adddate >> /var/log/jbosseap.install.log
+echo -e "\t stack UDP to TCP"           | adddate >> /var/log/jbosseap.install.log
+echo -e "\t management:inet-address"    | adddate >> /var/log/jbosseap.install.log
+echo -e "\t public:inet-address"        | adddate >> /var/log/jbosseap.install.log
+echo -e "\t private:inet-address"       | adddate >> /var/log/jbosseap.install.log
+echo -e "\t webservices:wsdl-host"      | adddate >> /var/log/jbosseap.install.log
+
+$EAP_HOME/bin/jboss-cli.sh --echo-command \
+'embed-server --std-out=echo  --server-config="${EAP_RPM_CONF_STANDALONE}"',\
+'/subsystem=jgroups/channel=ee:write-attribute(name="stack", value="tcp")',\
+'/interface=management:write-attribute(name=inet-address, value="${jboss.bind.address.management:0.0.0.0}")',\
+'/interface=public:write-attribute(name=inet-address, value="${jboss.bind.address:0.0.0.0}")',\
+'/interface=private:write-attribute(name=inet-address, value="${jboss.bind.address.private:0.0.0.0}")',\
+'/subsystem=webservices:write-attribute(name=wsdl-host, value="${jboss.bind.address:0.0.0.0}")' | adddate >> /var/log/jbosseap.install.log 2>&1
+
+####################### Start the JBoss server and setup eap service
+echo "Start JBoss-EAP service"                  | adddate >> /var/log/jbosseap.install.log
+echo "systemctl enable eap7-standalone.service" | adddate >> /var/log/jbosseap.install.log
+systemctl enable eap7-standalone.service        | adddate >> /var/log/jbosseap.install.log 2>&1
+
+echo "systemctl restart eap7-standalone.service"| adddate >> /var/log/jbosseap.install.log
+systemctl restart eap7-standalone.service       | adddate >> /var/log/jbosseap.install.log 2>&1
 echo "systemctl status eap7-standalone.service" | adddate >> /var/log/jbosseap.install.log
-systemctl status eap7-standalone.service | adddate >> /var/log/jbosseap.install.log 2>&1
+systemctl status eap7-standalone.service        | adddate >> /var/log/jbosseap.install.log 2>&1
+####################### 
 
 # Open Red Hat software firewall for port 8080 and 9990:
 openport 8080
