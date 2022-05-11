e12c4fcb85c814207f3bdcb95cb2a6447ee2fcd6
Ed Burns <edburns@microsoft.com>
2022-05-11 16:55:25 -0400

    On branch edburns-msft-77-remove-7-3-subdirectories deleted:    eap73-rhel8-byos-multivm/pom.xml deleted:    eap73-rhel8-byos-multivm/src/main/arm/createUiDefinition.json deleted:    eap73-rhel8-byos-multivm/src/main/arm/mainTemplate.json deleted:    eap73-rhel8-byos-multivm/src/main/bin/eap-session-replication.war deleted:    eap73-rhel8-byos-multivm/src/main/scripts/jbosseap-setup-redhat.sh deleted:    eap73-rhel8-byos-vmss/pom.xml deleted:    eap73-rhel8-byos-vmss/src/main/arm/createUiDefinition.json deleted:    eap73-rhel8-byos-vmss/src/main/arm/mainTemplate.json deleted:    eap73-rhel8-byos-vmss/src/main/bin/eap-session-replication.war deleted:    eap73-rhel8-byos-vmss/src/main/scripts/jbosseap-setup-redhat.sh deleted:    eap73-rhel8-byos/pom.xml deleted:    eap73-rhel8-byos/src/main/arm/createUiDefinition.json deleted:    eap73-rhel8-byos/src/main/arm/mainTemplate.json deleted:    eap73-rhel8-byos/src/main/bin/JBoss-EAP_on_Azure.war deleted:    eap73-rhel8-byos/src/main/scripts/jbosseap-setup-redhat.sh deleted:    eap73-rhel8-payg-multivm/pom.xml deleted:    eap73-rhel8-payg-multivm/src/main/arm/createUiDefinition.json deleted:    eap73-rhel8-payg-multivm/src/main/arm/mainTemplate.json deleted:    eap73-rhel8-payg-multivm/src/main/bin/eap-session-replication.war deleted:    eap73-rhel8-payg-multivm/src/main/scripts/jbosseap-setup-redhat.sh deleted:    eap73-rhel8-payg-vmss/pom.xml deleted:    eap73-rhel8-payg-vmss/src/main/arm/createUiDefinition.json deleted:    eap73-rhel8-payg-vmss/src/main/arm/mainTemplate.json deleted:    eap73-rhel8-payg-vmss/src/main/bin/eap-session-replication.war deleted:    eap73-rhel8-payg-vmss/src/main/scripts/jbosseap-setup-redhat.sh deleted:    eap73-rhel8-payg/pom.xml deleted:    eap73-rhel8-payg/src/main/arm/createUiDefinition.json deleted:    eap73-rhel8-payg/src/main/arm/mainTemplate.json deleted:    eap73-rhel8-payg/src/main/bin/JBoss-EAP_on_Azure.war deleted:    eap73-rhel8-payg/src/main/scripts/jbosseap-setup-redhat.sh modified:   CmdToRun.md modified:   README.md deleted:    Deploy-All.ps1 modified:   CmdToRun.md modified:   README.md

--- Deploy-All.ps1
+++
@@ -1,6 +0,0 @@
-.\Deploy-AzTemplate.ps1 -ArtifactStagingDirectory .\eap73-rhel8-byos -ResourceGroupLocation southeastasia -dev -UploadArtifacts
-.\Deploy-AzTemplate.ps1 -ArtifactStagingDirectory .\eap73-rhel8-byos-vmss -ResourceGroupLocation southeastasia -dev -UploadArtifacts
-.\Deploy-AzTemplate.ps1 -ArtifactStagingDirectory .\eap73-rhel8-byos-multivm -ResourceGroupLocation southeastasia -dev -UploadArtifacts
-.\Deploy-AzTemplate.ps1 -ArtifactStagingDirectory .\eap73-rhel8-payg -ResourceGroupLocation southeastasia -dev -UploadArtifacts
-.\Deploy-AzTemplate.ps1 -ArtifactStagingDirectory .\eap73-rhel8-payg-vmss -ResourceGroupLocation southeastasia -dev -UploadArtifacts
-.\Deploy-AzTemplate.ps1 -ArtifactStagingDirectory .\eap73-rhel8-payg-multivm -ResourceGroupLocation southeastasia -dev -UploadArtifacts
