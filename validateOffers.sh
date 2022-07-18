5869c6154d95991bb5cfe32636c151c2805c33f2
Zheng Chang <zhengchang@microsoft.com>
2022-07-18 11:14:14 +0800

    Convert to Bicep
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>

--- validateOffers.sh
+++ validateOffers.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-OFFER_PATH_PATTERN="eap74*"
+OFFER_PATH_PATTERN="eap74eap74-rhel8-byos*"
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR="$(readlink -f ${CURR_DIR})"
 
@@ -20,7 +20,7 @@ rc=0
 failureIndicator='\[-\]'
 for d in */ ; do
     folderName=$(basename $d)
-    if [[ $folderName == $OFFER_PATH_PATTERN && $folderName != 'eap74-rhel8-payg-multivm' && $folderName != 'eap74-rhel8-payg-vmss' ]]; then
+    if [[ $folderName == $OFFER_PATH_PATTERN ]]; then
         msg "${YELLOW}matched folder name: $folderName. Full path is: ${BASE_DIR}/${folderName}/src/main/arm ${NOFORMAT}"
         ./../arm-ttk/arm-ttk/Test-AzTemplate.sh -TemplatePath ${BASE_DIR}/${folderName}/src/main/arm | tee testOutput
         set +e ; grep -q ${failureIndicator} testOutput;
