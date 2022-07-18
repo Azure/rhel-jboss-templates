662f18240e6e07cf387de055a14a4a0cde0e89c5
Zheng Chang <zhengchang@microsoft.com>
2022-07-18 11:37:39 +0800

    bugfix
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>

--- eap74-rhel8-payg/src/test/scripts/gen-parameters.sh
+++ eap74-rhel8-payg/src/test/scripts/gen-parameters.sh
@@ -10,7 +10,7 @@ cat <<EOF > ${parametersPath}
     "contentVersion": "1.0.0.0",
     "parameters": {
         "artifactsLocation": {
-            "value": "https://raw.githubusercontent.com/${gitUserName}/rhel-jboss-templates/${testbranchName}/eap74-rhel8-payg-vmss/"
+            "value": "https://raw.githubusercontent.com/${gitUserName}/rhel-jboss-templates/${testbranchName}/eap74-rhel8-payg/"
         },
         "location": {
             "value": "${location}"
@@ -53,7 +53,7 @@ cat <<EOF > ${parametersPath}
         "bootDiagnostics": {
             "value": "on"
         },
-        "bootStorageNewOrExisting": {
+        "storageNewOrExisting": {
             "value": "New"
         },
         "storageAccountName": {
@@ -61,9 +61,6 @@ cat <<EOF > ${parametersPath}
         },
         "storageAccountKind": {
             "value": "Storage"
-        },
-        "bootStorageReplication": {
-            "value": "Standard_LRS"
         },
         "storageAccountResourceGroupName": {
             "value": "${storageAccountResourceGroupName}"
