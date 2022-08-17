d4095b8aa7acc744ee54f5a69f86c8772390952e
zhengchang907 <zhengchang@microsoft.com>
2022-08-18 06:29:38 +0800

    Fix issue 102: "VM extension script failed but deployment script continues without exiting" (#103)
    * Fix issue 102
    
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>
    
    * Add more exit and increase version
    
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>
    
    * Fix PAYG multivm
    
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>
    
    * Bugfix
    
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>
    
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>

--- eap74-rhel8-byos-multivm/src/main/scripts/jbosseap-setup-redhat.sh
+++ eap74-rhel8-byos-multivm/src/main/scripts/jbosseap-setup-redhat.sh
@@ -41,22 +41,10 @@ SATELLITE_VM_FQDN=${28}
 
 # Get storage account sas token
 STORAGE_ACCESS_KEY=$(az storage account keys list --verbose --account-name "${STORAGE_ACCOUNT_NAME}" --query [0].value --output tsv)
-
-echo "STORAGE_ACCESS_KEY: ${STORAGE_ACCESS_KEY}"
-
-privateEndpointId=$(az storage account show --resource-group ${RESOURCE_GROUP_NAME} --name ${STORAGE_ACCOUNT_NAME} --query privateEndpointConnections[0].privateEndpoint.id -o tsv)
-privateEndpointIp=$(az network private-endpoint show --ids $privateEndpointId --query customDnsConfigs[0].ipAddresses[0] -o tsv)
-
-# Get domain controller host private IP
-DOMAIN_CONTROLLER_PRIVATE_IP=$(az vm list-ip-addresses --verbose --resource-group ${RESOURCE_GROUP_NAME} --name "${VM_NAME_PREFIX}0" --query [0].virtualMachine.network.privateIpAddresses[0] --output tsv)
-
-echo "DOMAIN_CONTROLLER_PRIVATE_IP: ${DOMAIN_CONTROLLER_PRIVATE_IP}"
+if [[ -z "${STORAGE_ACCESS_KEY}" ]] ; then echo  "Failed to get storage account sas token"; exit 1;  fi
 
 # Markdown script location
 SCRIPT_LOCATION=${artifactsLocation}${pathToScript}
-
-echo "SCRIPT_LOCATION: ${SCRIPT_LOCATION}"
-
 
 if [ "${CONFIGURATION_MODE}" != "managed-domain" ]; then
     # Configure standalone host
@@ -69,10 +57,20 @@ if [ "${CONFIGURATION_MODE}" != "managed-domain" ]; then
         --version 2.0 \
         --settings "{\"fileUris\": [\"${SCRIPT_LOCATION}/jbosseap-setup-standalone.sh\"]}" \
         --protected-settings "{\"commandToExecute\":\"bash jbosseap-setup-standalone.sh -a $artifactsLocation -t $token -p $pathToFile -f $fileToDownload ${JBOSS_EAP_USER} ${JBOSS_EAP_PASSWORD_BASE64} ${RHSM_USER} ${RHSM_PASSWORD_BASE64} ${EAP_POOL} ${RHEL_POOL} ${STORAGE_ACCOUNT_NAME} ${CONTAINER_NAME} ${STORAGE_ACCESS_KEY} ${CONNECT_SATELLITE} ${SATELLITE_ACTIVATION_KEY_BASE64} ${SATELLITE_ORG_NAME_BASE64} ${SATELLITE_VM_FQDN} \"}"
-        echo $?
+        if [ $? != 0 ] ; then echo  "Failed to configure standalone host ${VM_NAME_PREFIX}${i}"; exit 1;  fi
         echo "standalone ${VM_NAME_PREFIX}${i} extension execution completed"
     done
 else
+    privateEndpointId=$(az storage account show --resource-group ${RESOURCE_GROUP_NAME} --name ${STORAGE_ACCOUNT_NAME} --query privateEndpointConnections[0].privateEndpoint.id -o tsv)
+    if [[ -z "${privateEndpointId}" ]] ; then echo  "Failed to get private endpoint ID"; exit 1;  fi
+
+    privateEndpointIp=$(az network private-endpoint show --ids $privateEndpointId --query customDnsConfigs[0].ipAddresses[0] -o tsv)
+    if [[ -z "${privateEndpointIp}" ]] ; then echo  "Failed to get private endpoint IP"; exit 1;  fi
+
+    # Get domain controller host private IP
+    DOMAIN_CONTROLLER_PRIVATE_IP=$(az vm list-ip-addresses --verbose --resource-group ${RESOURCE_GROUP_NAME} --name "${VM_NAME_PREFIX}0" --query [0].virtualMachine.network.privateIpAddresses[0] --output tsv)
+    if [[ -z "${DOMAIN_CONTROLLER_PRIVATE_IP}" ]] ; then echo  "Failed to get domain controller host private IP"; exit 1;  fi
+
     # Configure domain controller host
     echo "Configure domain controller host: ${VM_NAME_PREFIX}0"
 
@@ -83,9 +81,8 @@ else
         --version 2.0 \
         --settings "{\"fileUris\": [\"${SCRIPT_LOCATION}/jbosseap-setup-master.sh\"]}" \
         --protected-settings "{\"commandToExecute\":\"bash jbosseap-setup-master.sh -a $artifactsLocation -t $token -p $pathToFile -f $fileToDownload ${JBOSS_EAP_USER} ${JBOSS_EAP_PASSWORD_BASE64} ${RHSM_USER} ${RHSM_PASSWORD_BASE64} ${EAP_POOL} ${RHEL_POOL} ${STORAGE_ACCOUNT_NAME} ${CONTAINER_NAME} ${STORAGE_ACCESS_KEY} ${privateEndpointIp} ${CONNECT_SATELLITE} ${SATELLITE_ACTIVATION_KEY_BASE64} ${SATELLITE_ORG_NAME_BASE64} ${SATELLITE_VM_FQDN} \"}"
-    # error exception
-    echo $?
-    echo "Domain controller VM extension execution completed"
+        if [ $? != 0 ] ; then echo  "Failed to configure domain controller host: ${VM_NAME_PREFIX}0"; exit 1;  fi
+        echo "Domain controller VM extension execution completed"
 
     for ((i = 1; i < NUMBER_OF_INSTANCE; i++)); do
         echo "Configure domain slave host: ${VM_NAME_PREFIX}${i}"
@@ -96,7 +93,7 @@ else
         --version 2.0 \
         --settings "{\"fileUris\": [\"${SCRIPT_LOCATION}/jbosseap-setup-slave.sh\"]}" \
         --protected-settings "{\"commandToExecute\":\"bash jbosseap-setup-slave.sh -a $artifactsLocation -t $token -p $pathToFile -f $fileToDownload ${JBOSS_EAP_USER} ${JBOSS_EAP_PASSWORD_BASE64} ${RHSM_USER} ${RHSM_PASSWORD_BASE64} ${EAP_POOL} ${RHEL_POOL} ${STORAGE_ACCOUNT_NAME} ${CONTAINER_NAME} ${STORAGE_ACCESS_KEY} ${privateEndpointIp} ${DOMAIN_CONTROLLER_PRIVATE_IP} ${NUMBER_OF_SERVER_INSTANCE} ${CONNECT_SATELLITE} ${SATELLITE_ACTIVATION_KEY_BASE64} ${SATELLITE_ORG_NAME_BASE64} ${SATELLITE_VM_FQDN} \"}"
-        echo $?
+        if [ $? != 0 ] ; then echo  "Failed to configure domain slave host: ${VM_NAME_PREFIX}${i}"; exit 1;  fi
         echo "Slave ${VM_NAME_PREFIX}${i} extension execution completed"
     done
 fi
