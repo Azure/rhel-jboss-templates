5beb029055f1b6625aa031b73a81312f77ffc426
zhengchang907 <zhengchang@microsoft.com>
2022-11-30 07:41:52 +0800

    EAP on ARO (#128)
    * EAP on ARO
    
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>
    
    * On branch jbossaro Apply pid from partner center. Thanks @zhengchang907 for making it so clear and easy.
    modified:   eap-aro/src/main/bicep/mainTemplate.bicep
    
    Signed-off-by: Ed Burns <edburns@microsoft.com>
    
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>
    Signed-off-by: Ed Burns <edburns@microsoft.com>
    Co-authored-by: Ed Burns <edburns@microsoft.com>

---
+++ eap-aro/src/main/scripts/jboss-setup.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+# Define functions
+wait_login_complete() {
+    username=$1
+    password=$2
+    apiServerUrl="$3"
+    logFile=$4
+
+    cnt=0
+    oc login -u $username -p $password --server="$apiServerUrl" >> $logFile 2>&1
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+
+        echo "Login failed with ${username}, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc login -u $username -p $password --server="$apiServerUrl" >> $logFile 2>&1
+    done
+}
+
+wait_subscription_created() {
+    subscriptionName=$1
+    namespaceName=$2
+    deploymentYaml=$3
+    logFile=$4
+
+    cnt=0
+    oc get packagemanifests -n openshift-marketplace | grep -q ${subscriptionName}
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+
+        echo "Unable to get the operator package manifest ${subscriptionName} from OperatorHub, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc get packagemanifests -n openshift-marketplace | grep -q ${subscriptionName}
+    done
+
+    cnt=0
+    oc apply -f ${deploymentYaml} >> $logFile
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+
+        echo "Failed to create the operator subscription ${subscriptionName}, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc apply -f ${deploymentYaml} >> $logFile
+    done
+
+    cnt=0
+    oc get subscription ${subscriptionName} -n ${namespaceName} 2>/dev/null
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+
+        echo "Unable to get the operator subscription ${subscriptionName}, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc get subscription ${subscriptionName} -n ${namespaceName} 2>/dev/null
+    done
+    echo "Subscription ${subscriptionName} created." >> $logFile
+}
+
+wait_deployment_complete() {
+    deploymentName=$1
+    namespaceName=$2
+    logFile=$3
+
+    cnt=0
+    oc get deployment ${deploymentName} -n ${namespaceName} 2>/dev/null
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+
+        echo "Unable to get the deployment ${deploymentName}, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc get deployment ${deploymentName} -n ${namespaceName} 2>/dev/null
+    done
+}
+
+# Define variables
+logFile=deployment.log
+
+# Install utilities
+apk update
+apk add gettext
+apk add apache2-utils
+
+# Install the OpenShift CLI
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz -q -P ~
+mkdir ~/openshift
+tar -zxvf ~/openshift-client-linux.tar.gz -C ~/openshift
+echo 'export PATH=$PATH:~/openshift' >> ~/.bash_profile && source ~/.bash_profile
+
+# Sign in to cluster
+credentials=$(az aro list-credentials -g $RESOURCE_GROUP -n $CLUSTER_NAME -o json)
+kubeadminUsername=$(echo $credentials | jq -r '.kubeadminUsername')
+kubeadminPassword=$(echo $credentials | jq -r '.kubeadminPassword')
+apiServerUrl=$(az aro show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query 'apiserverProfile.url' -o tsv)
+consoleUrl=$(az aro show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query 'consoleProfile.url' -o tsv)
+wait_login_complete $kubeadminUsername $kubeadminPassword "$apiServerUrl" $logFile
+if [[ $? -ne 0 ]]; then
+  echo "Failed to sign into the cluster with ${kubeadminUsername}." >&2
+  exit 1
+fi
+
+# Create subscption and install operator
+wait_subscription_created eap openshift-operators eap-operator-sub.yaml ${logFile}
+
+if [[ $? -ne 0 ]]; then
+  echo "Failed to install the JBoss EAP Operator from the OperatorHub." >&2
+  exit 1
+fi
+
+# Check deployment is succeed
+wait_deployment_complete eap-operator openshift-operators ${logFile}
+if [[ $? -ne 0 ]]; then
+  echo "The JBoss EAP Operator is not available." >&2
+  exit 1
+fi
