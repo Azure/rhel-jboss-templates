b12b9d55178680357c14591478b717ac82901ceb
zhengchang907 <zhengchang@microsoft.com>
2023-01-30 10:07:55 +0800

    Support S2I application deployment (#148)
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>

--- eap-aro/src/main/scripts/jboss-setup.sh
+++ eap-aro/src/main/scripts/jboss-setup.sh
@@ -79,6 +79,58 @@ wait_subscription_created() {
     echo "Subscription ${subscriptionName} created." >> $logFile
 }
 
+wait_maven_build_complete() {
+    logFile=$1
+    cnt=0
+    isSuccess="false"
+    while [[ "${isSuccess,,}" == "false" ]];
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        while IFS= read -r line
+        do
+            if [[ $line == *"BUILD SUCCESS"* ]]; then
+                echo "Found BUILD SUCCESS in log" >> $logFile 
+                isSuccess="true"
+            fi
+        done <<< `oc logs buildconfig/eap-app-build-artifacts`
+        if [[ "${isSuccess,,}" == "false" ]]; then
+	        echo "Unable to confirm the Maven build progress, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile 
+	        sleep 5
+        fi
+    done
+    echo "Maven build completed." >> $logFile 
+}
+
+wait_image_push_complete() {
+    logFile=$1
+    cnt=0
+    isSuccess="false"
+    while [[ "${isSuccess,,}" == "false" ]];
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        while IFS= read -r line
+        do
+            if [[ $line == *"Push successful"* ]]; then
+                echo "Found Push successful in log" >> $logFile 
+                isSuccess="true"
+            fi
+        done <<< `oc logs buildconfig/eap-app`
+        if [[ "${isSuccess,,}" == "false" ]]; then
+	        echo "Unable to confirm the image push progress, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile 
+	        sleep 5
+        fi
+    done
+    echo "Image push completed." >> $logFile 
+}
+
 wait_deployment_complete() {
     deploymentName=$1
     namespaceName=$2
@@ -97,6 +149,160 @@ wait_deployment_complete() {
         echo "Unable to get the deployment ${deploymentName}, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
         sleep 5
         oc get deployment ${deploymentName} -n ${namespaceName} 2>/dev/null
+    done
+}
+
+wait_scalation_complete() {
+    deploymentConfigName=$1
+    namespaceName=$2
+    logFile=$3
+
+    cnt=0
+    read -r -a replicas <<< `oc get deploymentconfig ${deploymentConfigName} -n ${namespaceName} -o=jsonpath='{.spec.replicas}{" "}{.status.readyReplicas}{" "}{.status.availableReplicas}{" "}{.status.updatedReplicas}{"\n"}'`
+    while [[ ${#replicas[@]} -ne 4 || ${replicas[0]} != ${replicas[1]} || ${replicas[1]} != ${replicas[2]} || ${replicas[2]} != ${replicas[3]} ]]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        # Delete pods in ImagePullBackOff status
+        podIds=`oc get pod -n ${namespaceName} | grep ImagePullBackOff | awk '{print $1}'`
+        read -r -a podIds <<< `echo $podIds`
+        for podId in "${podIds[@]}"
+        do
+            echo "Delete pod ${podId} in ImagePullBackOff status" >> $logFile
+            oc delete pod ${podId} -n ${namespaceName}
+        done
+        sleep 5
+        echo "Wait until the deploymentConfig ${deploymentConfigName} completes, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        read -r -a replicas <<< `oc get deploymentconfig ${deploymentConfigName} -n ${namespaceName} -o=jsonpath='{.spec.replicas}{" "}{.status.readyReplicas}{" "}{.status.availableReplicas}{" "}{.status.updatedReplicas}{"\n"}'`
+    done
+    echo "Deployment ${deploymentConfigName} completed." >> $logFile
+}
+
+wait_route_available() {
+    routeName=$1
+    namespaceName=$2
+    logFile=$3
+    cnt=0
+    oc get route ${routeName} -n ${namespaceName} 2>/dev/null
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        echo "Unable to get the route ${routeName}, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc get route ${routeName} -n ${namespaceName} 2>/dev/null
+    done
+    cnt=0
+    appEndpoint=$(oc get route ${routeName} -n ${namespaceName} -o=jsonpath='{.spec.host}')
+    echo "appEndpoint is ${appEndpoint}"
+    while [[ -z $appEndpoint ]]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        sleep 5
+        echo "Wait until the host of route ${routeName} is available, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        appEndpoint=$(oc get route ${routeName} -n ${namespaceName} -o=jsonpath='{.spec.host}')
+        echo "appEndpoint is ${appEndpoint}"
+    done
+}
+
+wait_project_created() {
+    namespaceName=$1
+    logFile=$2
+    cnt=0
+    oc new-project ${namespaceName} 2>/dev/null
+    oc get project ${namespaceName} 2>/dev/null
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        echo "Unable to create the project ${namespaceName}, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc new-project ${namespaceName} 2>/dev/null
+        oc get project ${namespaceName} 2>/dev/null
+    done
+}
+
+wait_application_created() {
+    project_name=$1
+    src_repo_url=$2
+    src_repo_ref=$3
+    src_repo_dir=$4
+    logFile=$5
+    cnt=0
+    oc new-app --template=eap74-basic-s2i \
+        -p IMAGE_STREAM_NAMESPACE=${project_name} \
+        -p EAP_IMAGE_NAME=jboss-eap74-openjdk11-openshift:7.4.0 \
+        -p EAP_RUNTIME_IMAGE_NAME=jboss-eap74-openjdk11-runtime-openshift:7.4.0 \
+        -p SOURCE_REPOSITORY_URL=${src_repo_url} \
+        -p SOURCE_REPOSITORY_REF=${src_repo_ref} \
+        -p CONTEXT_DIR=${src_repo_dir} 2>/dev/null
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        echo "Unable to create the application, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc new-app --template=eap74-basic-s2i \
+            -p IMAGE_STREAM_NAMESPACE=${project_name} \
+            -p EAP_IMAGE_NAME=jboss-eap74-openjdk11-openshift:7.4.0 \
+            -p EAP_RUNTIME_IMAGE_NAME=jboss-eap74-openjdk11-runtime-openshift:7.4.0 \
+            -p SOURCE_REPOSITORY_URL=${src_repo_url} \
+            -p SOURCE_REPOSITORY_REF=${src_repo_ref} \
+            -p CONTEXT_DIR=${src_repo_dir} 2>/dev/null
+    done
+}
+
+wait_file_based_creation() {
+    deploymentFile=$1
+    logFile=$2
+    cnt=0
+    oc create -f ${deploymentFile} 2>/dev/null
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        echo "Unable to complete creation, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc create -f ${deploymentFile} 2>/dev/null
+    done
+}
+
+wait_secret_link() {
+    secretName=$1
+    logFile=$2
+    cnt=0
+    oc secrets link default ${secretName} --for=pull 2>/dev/null
+    oc secrets link builder ${secretName} --for=pull 2>/dev/null
+    while [ $? -ne 0 ]
+    do
+        if [ $cnt -eq $MAX_RETRIES ]; then
+            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
+            return 1
+        fi
+        cnt=$((cnt+1))
+        echo "Unable to secret link, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
+        sleep 5
+        oc secrets link default ${secretName} --for=pull 2>/dev/null
+        oc secrets link builder ${secretName} --for=pull 2>/dev/null
     done
 }
 
@@ -140,3 +346,120 @@ if [[ $? -ne 0 ]]; then
   echo "The JBoss EAP Operator is not available." >&2
   exit 1
 fi
+
+if [[ "${DEPLOY_APPLICATION,,}" == "true" ]]; then
+    # Create a new project for managing workload of the user
+    wait_project_created ${PROJECT_NAME} ${logFile}
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to create project ${PROJECT_NAME}." >&2
+        exit 1
+    fi
+    oc project ${PROJECT_NAME}
+
+    # Create secret YAML file
+    echo "Create secret YAML file" >> $logFile
+    secretDeploymentTemplate=red-hat-container-registry-pull-secret.yaml.template >> $logFile 
+    secretDeploymentFile=red-hat-container-registry-pull-secret.yaml >> $logFile 
+    envsubst < "$secretDeploymentTemplate" > "$secretDeploymentFile"
+
+    # Create secret
+    echo "Create secret" >> $logFile
+    wait_file_based_creation ${secretDeploymentFile} $logFile
+    if [[ $? != 0 ]]; then
+        echo "Failed to complete secret creation progress." >&2
+        exit 1
+    fi
+
+    # Configure the secret for project
+    echo "Configure the secret for project" >> $logFile
+    wait_secret_link ${CON_REG_SECRET_NAME}-pull-secret $logFile
+    if [[ $? != 0 ]]; then
+        echo "Failed to configure the secret for project." >&2
+        exit 1
+    fi
+
+    # Import command for JDK 11, we assume it is the first time
+    echo "Import command for JDK 11, we assume it is the first time" >> $logFile
+    eap74Openjdk11ImageStream="https://raw.githubusercontent.com/jboss-container-images/jboss-eap-openshift-templates/eap74/eap74-openjdk11-image-stream.json"
+    wait_file_based_creation ${eap74Openjdk11ImageStream} $logFile
+    if [[ $? != 0 ]]; then
+        echo "Failed to import command for JDK 11." >&2
+        exit 1
+    fi
+
+    # Import command for templates, we assume it is the first time
+    echo "Import command for templates, we assume it is the first time" >> $logFile
+    for resource in \
+        eap74-amq-persistent-s2i.json \
+        eap74-amq-s2i.json \
+        eap74-basic-s2i.json \
+        eap74-https-s2i.json \
+        eap74-sso-s2i.json
+
+    do
+        echo "Now import ${resource}" >> $logFile
+        resourceFile="https://raw.githubusercontent.com/jboss-container-images/jboss-eap-openshift-templates/eap74/templates/${resource}"
+        wait_file_based_creation ${resourceFile} $logFile
+        if [[ $? != 0 ]]; then
+            echo "Failed to import command for JDK 11." >&2
+            exit 1
+        fi
+    done
+
+    # Create a new application
+    echo "Create a new application" >> $logFile
+    wait_application_created ${PROJECT_NAME} ${SRC_REPO_URL} ${SRC_REPO_REF} ${SRC_REPO_DIR} $logFile
+    if [[ $? != 0 ]]; then
+        echo "Failed to complete application creation progress." >&2
+        exit 1
+    fi
+
+    # Check Maven build progress
+    echo "Check Maven build progress" >> $logFile
+    wait_maven_build_complete ${logFile}
+    if [[ $? != 0 ]]; then
+        echo "Failed to complete Maven build progress." >&2
+        exit 1
+    fi
+
+    # Check image push progress
+    echo "Check image push progress" >> $logFile
+    wait_image_push_complete ${logFile}
+    if [[ $? != 0 ]]; then
+        echo "Failed to complete image push progress." >&2
+        exit 1
+    fi
+
+    # Scale up the application instance
+    echo "Scale up the application instance" >> $logFile
+    oc scale deploymentconfig eap-app --replicas=${APP_REPLICAS} >> $logFile 
+    if [[ $? != 0 ]]; then
+        echo "Failed to kick off scalation progress." >&2
+        exit 1
+    fi
+
+    # Wait until the application scalation completes
+    wait_scalation_complete eap-app ${PROJECT_NAME} ${logFile}
+    if [[ $? != 0 ]]; then
+        echo "The application is not available." >> $logFile
+        exit 1
+    fi
+
+    # Get the route of the application
+    echo "Get the route of the application" >> $logFile
+    appEndpoint=
+    wait_route_available eap-app ${PROJECT_NAME} $logFile
+    if [[ $? -ne 0 ]]; then
+        echo "The route eap-app is not available." >> $logFile
+        exit 1
+    fi
+    echo "appEndpoint is ${appEndpoint}"
+fi
+
+# Write outputs to deployment script output path
+result=$(jq -n -c --arg consoleUrl $consoleUrl '{consoleUrl: $consoleUrl}')
+if [[ "${DEPLOY_APPLICATION,,}" == "true" ]]; then
+    result=$(echo "$result" | jq --arg appEndpoint "$appEndpoint" '{"appEndpoint": $appEndpoint} + .')
+fi
+echo "Result is: $result" >> $logFile
+echo $result > $AZ_SCRIPTS_OUTPUT_PATH
