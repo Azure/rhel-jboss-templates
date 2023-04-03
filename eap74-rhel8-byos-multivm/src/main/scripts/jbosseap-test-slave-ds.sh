fb2c5febc7751902154280eb3205a05f9a2a6560
Jianguo Ma <jiangma@microsoft.com>
2023-04-03 18:21:10 +0800

    try FQDN first
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- eap74-rhel8-byos-multivm/src/main/scripts/jbosseap-test-slave-ds.sh
+++ eap74-rhel8-byos-multivm/src/main/scripts/jbosseap-test-slave-ds.sh
@@ -13,23 +13,23 @@ DOMAIN_CONTROLLER_PRIVATE_IP=${3}
 NUMBER_OF_SERVER_INSTANCE=${4}
 jdbcDataSourceName=${5}
 
-HOST_VM_NAME=$(hostname)
-HOST_VM_NAME_LOWERCASES=$(echo "${HOST_VM_NAME,,}")
 FQDN=$(hostname -A)
 FQDN_LOWERCASES=$(echo "${FQDN,,}")
+HOST_VM_NAME=$(hostname)
+HOST_VM_NAME_LOWERCASES=$(echo "${HOST_VM_NAME,,}")
 
 # Load environment variables where EAP_HOME is defined
 source /etc/profile.d/eap_env.sh
 
-# Test connection for the created data source
 echo "Start to test data source connection" | log
 for ((i = 0; i < NUMBER_OF_SERVER_INSTANCE; i++)); do
+    # First try data source connection test using FQDN of the worker node
     sudo -u jboss $EAP_HOME/wildfly/bin/jboss-cli.sh --connect --controller=${DOMAIN_CONTROLLER_PRIVATE_IP} --user=${JBOSS_EAP_USER} --password=${JBOSS_EAP_PASSWORD} \
-        "/host=${HOST_VM_NAME_LOWERCASES}/server=${HOST_VM_NAME_LOWERCASES}-server${i}/subsystem=datasources/data-source=$jdbcDataSourceName:test-connection-in-pool" | log; flag=${PIPESTATUS[0]}
+        "/host=${FQDN_LOWERCASES}/server=${HOST_VM_NAME_LOWERCASES}-server${i}/subsystem=datasources/data-source=$jdbcDataSourceName:test-connection-in-pool" | log; flag=${PIPESTATUS[0]}
     if [ $flag != 0 ]; then
-        # Retry data source connection test using FQDN of the worker node
+        # Retry data source connection test using short hostname of the worker node
         sudo -u jboss $EAP_HOME/wildfly/bin/jboss-cli.sh --connect --controller=${DOMAIN_CONTROLLER_PRIVATE_IP} --user=${JBOSS_EAP_USER} --password=${JBOSS_EAP_PASSWORD} \
-            "/host=${FQDN_LOWERCASES}/server=${HOST_VM_NAME_LOWERCASES}-server${i}/subsystem=datasources/data-source=$jdbcDataSourceName:test-connection-in-pool" | log; flag=${PIPESTATUS[0]}
+            "/host=${HOST_VM_NAME_LOWERCASES}/server=${HOST_VM_NAME_LOWERCASES}-server${i}/subsystem=datasources/data-source=$jdbcDataSourceName:test-connection-in-pool" | log; flag=${PIPESTATUS[0]}
         if [ $flag != 0 ]; then 
             echo "ERROR! Test data source connection failed." >&2 log
             exit $flag
