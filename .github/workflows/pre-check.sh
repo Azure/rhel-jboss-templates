b25cc085d3f4f0f365394f76c0d93889a12aae8a
zhihaoguo <zhihaoguo@microsoft.com>
2024-05-17 13:31:10 +0800

    add comments to check shell scripts

--- .github/workflows/pre-check.sh
+++ .github/workflows/pre-check.sh
@@ -1,15 +1,25 @@
 # Check env
 
-## Check if the required tools are installed
+# ANSI color codes
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+## Check if the required tools are installed and logged in
+echo -e "${GREEN}To run this script, you need to have the following tools installed:${NC}"
+echo -e "${GREEN}1. yq${NC}"
+echo -e "${GREEN}2. Github cli (gh)${NC}"
+echo -e "${GREEN}3. Azure CLI (az)${NC}"
+echo -e "${GREEN}And you need to be logged in to GitHub CLI (gh), and Azure CLI (az).${NC}"
+
 echo "Checking if the required tools are installed..."
 echo "Checking progress started..."
 
-if ! command -v jq &> /dev/null; then
+if ! command -v yq &> /dev/null; then
     echo "Env Check Failed."
-    echo "jq is not installed. Please install it to proceed."
+    echo "yq is not installed. Please install it to proceed."
     exit 1
 fi
-echo "1/3...jq is installed."
+echo "1/5...jq is installed."
 
 # Check gh installed
 if ! command -v gh &> /dev/null; then
@@ -17,7 +27,7 @@ if ! command -v gh &> /dev/null; then
     echo "GitHub CLI (gh) is not installed. Please install it to proceed."
     exit 1
 fi
-echo "2/3...GitHub CLI (gh) is installed."
+echo "2/5...GitHub CLI (gh) is installed."
 
 
 # Check if the GitHub CLI (gh) is logged in
@@ -26,7 +36,24 @@ if ! gh auth status &> /dev/null; then
     echo "You are not logged in to GitHub CLI (gh). Please log in to proceed."
     exit 1
 fi
-echo "3/3...You are logged in to GitHub CLI (gh)."
+echo "3/5...You are logged in to GitHub CLI (gh)."
+
+# check if az is installed
+if ! command -v az &> /dev/null; then
+    echo "Env Check Failed."
+    echo "Azure CLI (az) is not installed. Please install it to proceed."
+    exit 1
+fi
+echo "4/5...Azure CLI (az) is installed."
+
+
+# check if az is logged in
+if ! az account show &> /dev/null; then
+    echo "Env Check Failed."
+    echo "You are not logged in to Azure CLI (az). Please log in to proceed."
+    exit 1
+fi
+echo "5/5...You are logged in to Azure CLI (az)."
 
 echo "Checking progress completed..."
 
