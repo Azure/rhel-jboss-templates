b274e707c526038a18d26aab6f925fd29b397c24
zhihaoguo <zhihaoguo@microsoft.com>
2024-06-21 13:26:07 +0800

    Updated tear-down-cluster-credentials.sh and setup-cluster-credentials.sh scripts to include detailed instructions for initializing repository secrets.

--- .github/workflows/tear-down-cluster-credentials.sh
+++ .github/workflows/tear-down-cluster-credentials.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+################################################
+# This script is invoked by a human who:
+# - can remove repository secrets in the github repo from which this file was cloned.
+# - has the gh client >= 2.0.0 installed.
+#
+# This script initializes the repo from which this file is was cloned
+# with the necessary secrets to run the workflows.
+# Steps to run the Script:
+# 1. Run gh auth login.
+# 2. Clone the repository.
+# 3. Run the script with the following command:
+#    bash .github/workflows/tear-down-cluster-credentials.sh
+# 4. The script will remove the required secrets in the repository.
+# 5. Check the repository secrets to verify that the secrets are removed.
+################################################
+
 set -Eeuo pipefail
 
 source pre-check.sh
