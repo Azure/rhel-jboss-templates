e6a1d03b3a325fa841709ed32b02ac04dd5eddaf
zhihaoguo <zhihaoguo@microsoft.com>
2024-07-05 10:29:41 +0800

    Updated validation workflow to include JDK version and database type in the name. Added JDK version parameter to the generation script.

--- eap74-rhel8-payg-multivm/src/test/scripts/gen-parameters.sh
+++ eap74-rhel8-payg-multivm/src/test/scripts/gen-parameters.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 #read arguments from stdin
-read parametersPath gitUserName testbranchName location vmName asName adminUsername password numberOfInstances operatingMode virtualNetworkResourceGroupName bootStorageAccountName storageAccountResourceGroupName jbossEAPUserName jbossEAPPassword enableDB databaseType jdbcDataSourceJNDIName dsConnectionURL dbUser dbPassword
+read parametersPath gitUserName testbranchName location vmName asName adminUsername password numberOfInstances operatingMode virtualNetworkResourceGroupName bootStorageAccountName storageAccountResourceGroupName jbossEAPUserName jbossEAPPassword enableDB databaseType jdbcDataSourceJNDIName dsConnectionURL dbUser dbPassword jdkVersion
  
 cat <<EOF > ${parametersPath}
 {
@@ -109,6 +109,9 @@ cat <<EOF > ${parametersPath}
         },
         "dbPassword": {
             "value": "${dbPassword}"
+        },
+        "jdkVersion": {
+            "value": "${jdkVersion}"
         }
     }
 }
