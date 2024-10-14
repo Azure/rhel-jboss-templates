b25a876b89dd9fc149dfddbae8b92ce7613ab473
Ed Burns <edburns@microsoft.com>
2024-10-14 19:31:03 -0400

    On branch eap8-byos modified:   generateOffers.sh
    - Bash best practice.
    
    - Update for new directory names.
    
    Signed-off-by: Ed Burns <edburns@microsoft.com>

--- generateOffers.sh
+++ generateOffers.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+set -Eeuo pipefail
 
-OFFER_PATH_PATTERN="eap74*"
+OFFER_PATH_PATTERN="eap*rhel*"
 
 setup_colors() {
   NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
