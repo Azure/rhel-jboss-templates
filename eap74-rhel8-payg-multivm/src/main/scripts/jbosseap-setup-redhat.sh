3fc751725b25ead1809061d938e9d33644dcd190
zhihaoguo <zhihaoguo@microsoft.com>
2025-01-20 14:32:03 +0800

    Add centralized logging function and integrate it into key script actions.

--- eap74-rhel8-payg-multivm/src/main/scripts/jbosseap-setup-redhat.sh
+++ eap74-rhel8-payg-multivm/src/main/scripts/jbosseap-setup-redhat.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+log() {
+    while IFS= read -r line; do
+        printf '%s %s\n' "$(date "+%Y-%m-%d %H:%M:%S")" "$line" >> /var/log/jbosseap.install.log
+    done
+}
+
 # Get storage account sas token
 STORAGE_ACCESS_KEY=$(az storage account keys list --verbose --account-name "${STORAGE_ACCOUNT_NAME}" --query [0].value --output tsv)
 if [[ -z "${STORAGE_ACCESS_KEY}" ]] ; then echo "Failed to get storage account sas token"; exit 1; fi
@@ -19,10 +25,10 @@ if [ "${CONFIGURATION_MODE}" != "managed-domain" ]; then
         # Configure standalone host
         # Update the IP configuration of network interface and set its private ip allocation method to Static
         ipConfigName=$(az network nic show -g ${RESOURCE_GROUP_NAME} -n ${NIC_NAME}${i} --query 'ipConfigurations[0].name' -o tsv)
-        echo "Set private ip allocation method to Static for host: ${VM_NAME_PREFIX}${i}"
+        echo "Set private ip allocation method to Static for host: ${VM_NAME_PREFIX}${i}" | log; flag=${PIPESTATUS[0]}
         az network nic ip-config update -g ${RESOURCE_GROUP_NAME} --nic-name ${NIC_NAME}${i} -n ${ipConfigName} --set privateIpAllocationMethod=Static
         
-        echo "Configure standalone host: ${VM_NAME_PREFIX}${i}"
+        echo "Configure standalone host: ${VM_NAME_PREFIX}${i}" | log; flag=${PIPESTATUS[0]}
         az vm extension set --verbose --name CustomScript \
         --resource-group ${RESOURCE_GROUP_NAME} \
         --vm-name ${VM_NAME_PREFIX}${i} \
@@ -31,28 +37,28 @@ if [ "${CONFIGURATION_MODE}" != "managed-domain" ]; then
         --settings "{\"fileUris\": [\"${SCRIPT_LOCATION}/jbosseap-setup-standalone.sh\", \"${postgresqlDSScriptUri}\", \"${mssqlserverDSScriptUri}\", \"${oracleDSScriptUri}\", \"${mysqlDSScriptUri}\"]}" \
         --protected-settings "{\"commandToExecute\":\"sh jbosseap-setup-standalone.sh -a ${ARTIFACTS_LOCATION} -t ${ARTIFACTS_LOCATION_SAS_TOKEN} -p ${PATH_TO_FILE} -f ${FILE_TO_DOWNLOAD} ${JBOSS_EAP_USER} ${JBOSS_EAP_PASSWORD_BASE64} ${RHSM_USER} ${RHSM_PASSWORD_BASE64} ${EAP_POOL} ${JDK_VERSION} ${STORAGE_ACCOUNT_NAME} ${CONTAINER_NAME} ${STORAGE_ACCESS_KEY} ${CONNECT_SATELLITE} ${SATELLITE_ACTIVATION_KEY_BASE64} ${SATELLITE_ORG_NAME_BASE64} ${SATELLITE_VM_FQDN} ${ENABLE_DB} ${DATABASE_TYPE} ${JDBC_DATA_SOURCE_JNDI_NAME_BASE64} ${DS_CONNECTION_URL_BASE64} ${DB_USER_BASE64} ${DB_PASSWORD_BASE64}\"}"
         if [ $? != 0 ] ; then echo "Failed to configure standalone host ${VM_NAME_PREFIX}${i}"; exit 1; fi
-        echo "standalone ${VM_NAME_PREFIX}${i} extension execution completed"
+        echo "standalone ${VM_NAME_PREFIX}${i} extension execution completed" | log; flag=${PIPESTATUS[0]}
     done
 else
     enableElytronSe17DomainCliUri="${SCRIPT_LOCATION}/enable-elytron-se17-domain.cli"
 
     privateEndpointId=$(az storage account show --resource-group ${RESOURCE_GROUP_NAME} --name ${STORAGE_ACCOUNT_NAME} --query privateEndpointConnections[0].privateEndpoint.id -o tsv)
-    if [[ -z "${privateEndpointId}" ]] ; then echo "Failed to get private endpoint ID"; exit 1; fi
+    if [[ -z "${privateEndpointId}" ]] ; then echo "Failed to get private endpoint ID" | log; flag=${PIPESTATUS[0]}; exit 1; fi
 
     privateEndpointIp=$(az network private-endpoint show --ids $privateEndpointId --query customDnsConfigs[0].ipAddresses[0] -o tsv)
-    if [[ -z "${privateEndpointIp}" ]] ; then echo "Failed to get private endpoint IP"; exit 1; fi
+    if [[ -z "${privateEndpointIp}" ]] ; then echo "Failed to get private endpoint IP" | log; flag=${PIPESTATUS[0]}; exit 1; fi
 
     # Get domain controller host private IP
     DOMAIN_CONTROLLER_PRIVATE_IP=$(az vm list-ip-addresses --verbose --resource-group ${RESOURCE_GROUP_NAME} --name "${ADMIN_VM_NAME}" --query [0].virtualMachine.network.privateIpAddresses[0] --output tsv)
-    if [[ -z "${DOMAIN_CONTROLLER_PRIVATE_IP}" ]] ; then echo "Failed to get domain controller host private IP"; exit 1; fi
+    if [[ -z "${DOMAIN_CONTROLLER_PRIVATE_IP}" ]] ; then echo "Failed to get domain controller host private IP" | log; flag=${PIPESTATUS[0]}; exit 1; fi
 
     # Update the IP configuration of network interface and set its private ip allocation method to Static
     ipConfigName=$(az network nic show -g ${RESOURCE_GROUP_NAME} -n ${NIC_NAME}0 --query 'ipConfigurations[0].name' -o tsv)
-    echo "Set private ip allocation method to Static for host: ${ADMIN_VM_NAME}"
+    echo "Set private ip allocation method to Static for host: ${ADMIN_VM_NAME}" | log; flag=${PIPESTATUS[0]}
     az network nic ip-config update -g ${RESOURCE_GROUP_NAME} --nic-name ${NIC_NAME}0 -n ${ipConfigName} --set privateIpAllocationMethod=Static
 
     # Configure domain controller host
-    echo "Configure domain controller host: ${ADMIN_VM_NAME}"
+    echo "Configure domain controller host: ${ADMIN_VM_NAME}" | log; flag=${PIPESTATUS[0]}
 
     az vm extension set --verbose --name CustomScript \
         --resource-group ${RESOURCE_GROUP_NAME} \
@@ -62,16 +68,16 @@ else
         --settings "{\"fileUris\": [\"${SCRIPT_LOCATION}/jbosseap-setup-master.sh\", \"${enableElytronSe17DomainCliUri}\"]}" \
         --protected-settings "{\"commandToExecute\":\"sh jbosseap-setup-master.sh ${JBOSS_EAP_USER} ${JBOSS_EAP_PASSWORD_BASE64} ${RHSM_USER} ${RHSM_PASSWORD_BASE64} ${EAP_POOL} ${JDK_VERSION} ${STORAGE_ACCOUNT_NAME} ${CONTAINER_NAME} ${STORAGE_ACCESS_KEY} ${privateEndpointIp} ${CONNECT_SATELLITE} ${SATELLITE_ACTIVATION_KEY_BASE64} ${SATELLITE_ORG_NAME_BASE64} ${SATELLITE_VM_FQDN}\"}"
         # error exception
-        if [ $? != 0 ] ; then echo "Failed to configure domain controller host: ${ADMIN_VM_NAME}"; exit 1; fi
-        echo "Domain controller VM extension execution completed"
+        if [ $? != 0 ] ; then echo "Failed to configure domain controller host: ${ADMIN_VM_NAME}" | log; flag=${PIPESTATUS[0]}; exit 1; fi
+        echo "Domain controller VM extension execution completed" | log; flag=${PIPESTATUS[0]}
 
     for ((i = 1; i < NUMBER_OF_INSTANCE; i++)); do
         # Update the IP configuration of network interface and set its private ip allocation method to Static
         ipConfigName=$(az network nic show -g ${RESOURCE_GROUP_NAME} -n ${NIC_NAME}${i} --query 'ipConfigurations[0].name' -o tsv)
-        echo "Set private ip allocation method to Static for host: ${VM_NAME_PREFIX}${i}"
+        echo "Set private ip allocation method to Static for host: ${VM_NAME_PREFIX}${i}" | log; flag=${PIPESTATUS[0]}
         az network nic ip-config update -g ${RESOURCE_GROUP_NAME} --nic-name ${NIC_NAME}${i} -n ${ipConfigName} --set privateIpAllocationMethod=Static
 
-        echo "Configure domain slave host: ${VM_NAME_PREFIX}${i}"
+        echo "Configure domain slave host: ${VM_NAME_PREFIX}${i}" | log; flag=${PIPESTATUS[0]}
         az vm extension set --verbose --name CustomScript \
         --resource-group ${RESOURCE_GROUP_NAME} \
         --vm-name ${VM_NAME_PREFIX}${i} \
@@ -79,13 +85,13 @@ else
         --version 2.0 \
         --settings "{\"fileUris\": [\"${SCRIPT_LOCATION}/jbosseap-setup-slave.sh\", \"${enableElytronSe17DomainCliUri}\", \"${postgresqlDSScriptUri}\", \"${mssqlserverDSScriptUri}\", \"${oracleDSScriptUri}\", \"${mysqlDSScriptUri}\"]}" \
         --protected-settings "{\"commandToExecute\":\"sh jbosseap-setup-slave.sh ${JBOSS_EAP_USER} ${JBOSS_EAP_PASSWORD_BASE64} ${RHSM_USER} ${RHSM_PASSWORD_BASE64} ${EAP_POOL} ${JDK_VERSION} ${STORAGE_ACCOUNT_NAME} ${CONTAINER_NAME} ${STORAGE_ACCESS_KEY} ${privateEndpointIp} ${DOMAIN_CONTROLLER_PRIVATE_IP} ${NUMBER_OF_SERVER_INSTANCE} ${CONNECT_SATELLITE} ${SATELLITE_ACTIVATION_KEY_BASE64} ${SATELLITE_ORG_NAME_BASE64} ${SATELLITE_VM_FQDN} ${ENABLE_DB} ${DATABASE_TYPE} ${JDBC_DATA_SOURCE_JNDI_NAME_BASE64} ${DS_CONNECTION_URL_BASE64} ${DB_USER_BASE64} ${DB_PASSWORD_BASE64}\"}"
-        if [ $? != 0 ] ; then echo "Failed to configure domain slave host: ${VM_NAME_PREFIX}${i}"; exit 1; fi
-        echo "Slave ${VM_NAME_PREFIX}${i} extension execution completed"
+        if [ $? != 0 ] ; then echo "Failed to configure domain slave host: ${VM_NAME_PREFIX}${i}" | log; flag=${PIPESTATUS[0]}; exit 1; fi
+        echo "Slave ${VM_NAME_PREFIX}${i} extension execution completed" | log; flag=${PIPESTATUS[0]}
     done
 
     if [ "$ENABLE_DB" == "True" ]; then
         # Configure data source
-        echo "Configure data source for host: ${ADMIN_VM_NAME}"
+        echo "Configure data source for host: ${ADMIN_VM_NAME}" | log; flag=${PIPESTATUS[0]}
         masterDSScriptUri="${SCRIPT_LOCATION}/jbosseap-setup-master-ds.sh"
         jdbcDataSourceName=dataSource-$DATABASE_TYPE
         az vm extension set --verbose --name CustomScript \
@@ -95,12 +101,12 @@ else
             --version 2.0 \
             --settings "{\"fileUris\": [\"${masterDSScriptUri}\", \"${postgresqlDSScriptUri}\", \"${mssqlserverDSScriptUri}\", \"${oracleDSScriptUri}\", \"${mysqlDSScriptUri}\"]}" \
             --protected-settings "{\"commandToExecute\":\"sh jbosseap-setup-master-ds.sh ${DATABASE_TYPE} ${jdbcDataSourceName} ${JDBC_DATA_SOURCE_JNDI_NAME_BASE64} ${DS_CONNECTION_URL_BASE64} ${DB_USER_BASE64} ${DB_PASSWORD_BASE64}\"}"
-            if [ $? != 0 ] ; then echo  "Failed to configure data source for host: ${ADMIN_VM_NAME}"; exit 1;  fi
-            echo "Data source configuration VM extension execution completed"
+            if [ $? != 0 ] ; then echo  "Failed to configure data source for host: ${ADMIN_VM_NAME}" | log; flag=${PIPESTATUS[0]}; exit 1;  fi
+            echo "Data source configuration VM extension execution completed" | log; flag=${PIPESTATUS[0]}
 
         # Test data source connection in worker nodes
         for ((i = 1; i < NUMBER_OF_INSTANCE; i++)); do
-            echo "Test data source connection in workder node: ${VM_NAME_PREFIX}${i}"
+            echo "Test data source connection in workder node: ${VM_NAME_PREFIX}${i}" | log; flag=${PIPESTATUS[0]}
             slaveTestDSConnScriptUri="${SCRIPT_LOCATION}/jbosseap-test-slave-ds.sh"
             az vm extension set --verbose --name CustomScript \
             --resource-group ${RESOURCE_GROUP_NAME} \
@@ -109,8 +115,8 @@ else
             --version 2.0 \
             --settings "{\"fileUris\": [\"${slaveTestDSConnScriptUri}\"]}" \
             --protected-settings "{\"commandToExecute\":\"sh jbosseap-test-slave-ds.sh ${JBOSS_EAP_USER} ${JBOSS_EAP_PASSWORD_BASE64} ${DOMAIN_CONTROLLER_PRIVATE_IP} ${NUMBER_OF_SERVER_INSTANCE} ${jdbcDataSourceName}\"}"
-            if [ $? != 0 ] ; then echo  "Test data source connection failed for worker node: ${VM_NAME_PREFIX}${i}"; exit 1;  fi
-            echo "Worker node ${VM_NAME_PREFIX}${i} data source connection test extension execution completed"
+            if [ $? != 0 ] ; then echo  "Test data source connection failed for worker node: ${VM_NAME_PREFIX}${i}" | log; flag=${PIPESTATUS[0]}; exit 1;  fi
+            echo "Worker node ${VM_NAME_PREFIX}${i} data source connection test extension execution completed" | log; flag=${PIPESTATUS[0]}
         done
     fi    
 fi
