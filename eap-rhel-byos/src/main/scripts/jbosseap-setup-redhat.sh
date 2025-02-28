6ded185fbff977dc7258e734216274208a189812
zhihaoguo <zhihaoguo@microsoft.com>
2025-02-28 15:07:46 +0800

    Refactor OpenJDK 17 conditional logic in JBoss EAP setup scripts

--- eap-rhel-byos/src/main/scripts/jbosseap-setup-redhat.sh
+++ eap-rhel-byos/src/main/scripts/jbosseap-setup-redhat.sh
@@ -156,7 +156,7 @@ echo -e "\t stack UDP to TCP"       | log; flag=${PIPESTATUS[0]}
 echo -e "\t set transaction id"     | log; flag=${PIPESTATUS[0]}
 
 ## OpenJDK 17 specific logic
-if [[ "${JDK_VERSION,,}" == "eap74-openjdk17" || "${JDK_VERSION,,}" == "eap8-openjdk17" ]]; then
+if [[ "${JDK_VERSION,,}" == "eap74-openjdk17" ]]; then
     sudo -u jboss $EAP_HOME/bin/jboss-cli.sh --file=$EAP_HOME/docs/examples/enable-elytron-se17.cli -Dconfig=standalone-full-ha.xml
 fi
 
