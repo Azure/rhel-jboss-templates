6a13ce376309403e68dd61d5d8423791ec79c422
zhihaoguo <zhihaoguo@microsoft.com>
2025-03-31 13:44:35 +0800

    Update createUiDefinition.json and jboss-setup.sh for JBoss EAP 8 integration; add helm.yaml for deployment configuration

--- eap-aro/src/main/scripts/jboss-setup.sh
+++ eap-aro/src/main/scripts/jboss-setup.sh
@@ -79,60 +79,6 @@ wait_subscription_created() {
     echo "Subscription ${subscriptionName} created." >> $logFile
 }
 
-wait_maven_build_complete() {
-    application_name=$1
-    logFile=$2
-    cnt=0
-    isSuccess="false"
-    while [[ "${isSuccess,,}" == "false" ]];
-    do
-        if [ $cnt -eq $MAX_RETRIES ]; then
-            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
-            return 1
-        fi
-        cnt=$((cnt+1))
-        while IFS= read -r line
-        do
-            if [[ $line == *"BUILD SUCCESS"* ]]; then
-                echo "Found BUILD SUCCESS in log" >> $logFile 
-                isSuccess="true"
-            fi
-        done <<< `oc logs buildconfig/${application_name}-build-artifacts`
-        if [[ "${isSuccess,,}" == "false" ]]; then
-	        echo "Unable to confirm the Maven build progress, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile 
-	        sleep 5
-        fi
-    done
-    echo "Maven build completed." >> $logFile 
-}
-
-wait_image_push_complete() {
-    application_name=$1
-    logFile=$2
-    cnt=0
-    isSuccess="false"
-    while [[ "${isSuccess,,}" == "false" ]];
-    do
-        if [ $cnt -eq $MAX_RETRIES ]; then
-            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
-            return 1
-        fi
-        cnt=$((cnt+1))
-        while IFS= read -r line
-        do
-            if [[ $line == *"Push successful"* ]]; then
-                echo "Found Push successful in log" >> $logFile 
-                isSuccess="true"
-            fi
-        done <<< `oc logs buildconfig/${application_name}`
-        if [[ "${isSuccess,,}" == "false" ]]; then
-	        echo "Unable to confirm the image push progress, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile 
-	        sleep 5
-        fi
-    done
-    echo "Image push completed." >> $logFile 
-}
-
 wait_deployment_complete() {
     deploymentName=$1
     namespaceName=$2
@@ -152,35 +98,6 @@ wait_deployment_complete() {
         sleep 5
         oc get deployment ${deploymentName} -n ${namespaceName} 2>/dev/null
     done
-}
-
-wait_image_deployment_complete() {
-    application_name=$1
-    project_name=$2
-    logFile=$3
-
-    cnt=0
-    read -r -a replicas <<< `oc get wildflyserver ${application_name} -n ${project_name} -o=jsonpath='{.spec.replicas}{" "}{.status.replicas}{"\n"}'`
-    while [[ ${#replicas[@]} -ne 2 || ${replicas[0]} != ${replicas[1]} ]]
-    do
-        if [ $cnt -eq $MAX_RETRIES ]; then
-            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
-            return 1
-        fi
-        cnt=$((cnt+1))
-        # Delete pods in ImagePullBackOff status
-        podIds=`oc get pod -n ${project_name} | grep ImagePullBackOff | awk '{print $1}'`
-        read -r -a podIds <<< `echo $podIds`
-        for podId in "${podIds[@]}"
-        do
-            echo "Delete pod ${podId} in ImagePullBackOff status" >> $logFile
-            oc delete pod ${podId} -n ${project_name}
-        done
-        sleep 5
-        echo "Wait until the deploymentConfig ${application_name} completes, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
-        read -r -a replicas <<< `oc get wildflyserver ${application_name} -n ${project_name} -o=jsonpath='{.spec.replicas}{" "}{.status.replicas}{"\n"}'`
-    done
-    echo "Deployment ${application_name} completed." >> $logFile
 }
 
 wait_route_available() {
@@ -273,41 +190,7 @@ wait_add_scc_privileged() {
     done
 }
 
-wait_application_image_created() {
-    project_name=$1
-    application_name=$2
-    src_repo_url=$3
-    src_repo_ref=$4
-    src_repo_dir=$5
-    logFile=$6
-    cnt=0
-    oc process eap-s2i-build \
-        -p APPLICATION_IMAGE=${application_name} \
-        -p EAP_IMAGE=jboss-eap74-openjdk11-openshift:7.4.0 \
-        -p EAP_RUNTIME_IMAGE=jboss-eap74-openjdk11-runtime-openshift:7.4.0 \
-        -p EAP_IMAGESTREAM_NAMESPACE=${project_name} \
-        -p SOURCE_REPOSITORY_URL=${src_repo_url} \
-        -p SOURCE_REPOSITORY_REF=${src_repo_ref} \
-        -p CONTEXT_DIR=${src_repo_dir} | oc apply -f - 2>/dev/null
-    while [ $? -ne 0 ]
-    do
-        if [ $cnt -eq $MAX_RETRIES ]; then
-            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
-            return 1
-        fi
-        cnt=$((cnt+1))
-        echo "Unable to create the application image, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
-        sleep 5
-        oc process eap-s2i-build \
-            -p APPLICATION_IMAGE=${application_name} \
-            -p EAP_IMAGE=jboss-eap74-openjdk11-openshift:7.4.0 \
-            -p EAP_RUNTIME_IMAGE=jboss-eap74-openjdk11-runtime-openshift:7.4.0 \
-            -p EAP_IMAGESTREAM_NAMESPACE=${project_name} \
-            -p SOURCE_REPOSITORY_URL=${src_repo_url} \
-            -p SOURCE_REPOSITORY_REF=${src_repo_ref} \
-            -p CONTEXT_DIR=${src_repo_dir} | oc apply -f - 2>/dev/null
-    done
-}
+
 
 wait_file_based_creation() {
     deploymentFile=$1
@@ -324,24 +207,6 @@ wait_file_based_creation() {
         echo "Unable to apply file, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
         sleep 5
         oc apply -f ${deploymentFile} 2>/dev/null
-    done
-}
-
-wait_file_based_replacement() {
-    templateFile=$1
-    logFile=$2
-    cnt=0
-    oc replace --force -f ${templateFile} 2>/dev/null
-    while [ $? -ne 0 ]
-    do
-        if [ $cnt -eq $MAX_RETRIES ]; then
-            echo "Timeout and exit due to the maximum retries reached." >> $logFile 
-            return 1
-        fi
-        cnt=$((cnt+1))
-        echo "Unable to complete replacement, retry ${cnt} of ${MAX_RETRIES}..." >> $logFile
-        sleep 5
-        oc replace --force -f ${templateFile} 2>/dev/null
     done
 }
 
@@ -419,6 +284,14 @@ if [[ $? -ne 0 ]]; then
 fi
 
 if [[ "${DEPLOY_APPLICATION,,}" == "true" ]]; then
+    # Install the Helm CLI
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    sudo chmod 700 get_helm.sh
+    ./get_helm.sh
+
+    # Add the JBoss EAP Helm chart repository
+    helm repo add jboss-eap https://jbossas.github.io/eap-charts/
+
     # Create a new project for managing workload of the user
     wait_project_created ${PROJECT_NAME} ${logFile}
     if [[ $? -ne 0 ]]; then
@@ -464,65 +337,7 @@ if [[ "${DEPLOY_APPLICATION,,}" == "true" ]]; then
         exit 1
     fi
 
-    # Import command for JDK 11, we assume it is the first time
-    echo "Import command for JDK 11, we assume it is the first time" >> $logFile
-    eap74Openjdk11ImageStream="https://raw.githubusercontent.com/jboss-container-images/jboss-eap-openshift-templates/eap74/eap74-openjdk11-image-stream.json"
-    wait_file_based_creation ${eap74Openjdk11ImageStream} $logFile
-    if [[ $? != 0 ]]; then
-        echo "Failed to import command for JDK 11." >&2
-        exit 1
-    fi
-
-    # Import eap-s2i-build template
-    echo "Import eap-s2i-build template" >> $logFile
-    eapS2iBuildTemplate="https://raw.githubusercontent.com/jboss-container-images/jboss-eap-openshift-templates/master/eap-s2i-build.yaml"
-    wait_file_based_replacement ${eapS2iBuildTemplate} $logFile
-    if [[ $? != 0 ]]; then
-        echo "Failed to import eap-s2i-build template." >&2
-        exit 1
-    fi
-
-    # Create a new application image
-    echo "Create a new application image" >> $logFile
-    wait_application_image_created ${PROJECT_NAME} ${APPLICATION_NAME} ${SRC_REPO_URL} ${SRC_REPO_REF} ${SRC_REPO_DIR} $logFile
-    if [[ $? != 0 ]]; then
-        echo "Failed to complete application image creation progress." >&2
-        exit 1
-    fi
-
-    # Check Maven build progress
-    echo "Check Maven build progress" >> $logFile
-    wait_maven_build_complete ${APPLICATION_NAME} ${logFile}
-    if [[ $? != 0 ]]; then
-        echo "Failed to complete Maven build progress." >&2
-        exit 1
-    fi
-
-    # Check image push progress
-    echo "Check image push progress" >> $logFile
-    wait_image_push_complete ${APPLICATION_NAME} ${logFile}
-    if [[ $? != 0 ]]; then
-        echo "Failed to complete image push progress." >&2
-        exit 1
-    fi
-    
-    # Create image deployment YAML file
-    echo "Create image deployment YAML file" >> $logFile
-    appDeploymentTemplate=app-deployment.yaml.template >> $logFile
-    appDeploymentFile=app-deployment.yaml >> $logFile
-    envsubst < "$appDeploymentTemplate" > "$appDeploymentFile"
-
-    # Apply image deployment file
-    echo "Apply image deployment file" >> $logFile
-    wait_file_based_creation ${appDeploymentFile} ${logFile}
-    if [[ $? != 0 ]]; then
-        echo "Failed to apply image deployment file." >&2
-        exit 1
-    fi
-
-    # Wait image deployment
-    echo "Wait image deployment" >> $logFile
-    wait_image_deployment_complete ${APPLICATION_NAME} ${PROJECT_NAME} $logFile
+    helm install ${APPLICATION_NAME} -f helm.yaml jboss-eap/eap8
 
     # Get the route of the application
     echo "Get the route of the application" >> $logFile
