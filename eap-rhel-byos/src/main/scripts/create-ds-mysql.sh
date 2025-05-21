6ea2b05b13845e395e6793c6f3b5fbbf20007248
zhihaoguo <zhihaoguo@microsoft.com>
2025-05-21 16:10:13 +0800

    Add support for passwordless MySQL connection and update UAMI display name reference

--- eap-rhel-byos/src/main/scripts/create-ds-mysql.sh
+++ eap-rhel-byos/src/main/scripts/create-ds-mysql.sh
@@ -14,19 +14,83 @@ jdbcDSJNDIName=$(echo "${3}" | base64 -d)           # JDBC Datasource JNDI name
 dsConnectionString=$(echo "${4}" | base64 -d)       # JDBC Datasource connection String
 databaseUser=$(echo "${5}" | base64 -d)             # Database username
 databasePassword=$(echo "${6}" | base64 -d)         # Database user password
+enablePswlessConnection=${7}                        # Enable passwordless connection
+uamiDisplayname=${8}                                # UAMI display name
 
-# Create JDBC driver and module directory
-jdbcDriverModuleDirectory="$eapRootPath"/modules/com/mysql/main
-mkdir -p "$jdbcDriverModuleDirectory"
 
-# Download JDBC driver
-jdbcDriverVersion=8.0.32
-jdbcDriverName=mysql-connector-j-${jdbcDriverVersion}.jar
-curl --retry 5 -Lo ${jdbcDriverModuleDirectory}/${jdbcDriverName} https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/${jdbcDriverVersion}/${jdbcDriverName}
+if [ "$(echo "$enablePswlessConnection" | tr '[:upper:]' '[:lower:]')" = "true" ]; then
+  echo "enablePswlessConnection=true, creating passwordless connection" | log
+      # Create JDBC driver and module directory
+      jdbcDriverModuleDirectory="$eapRootPath"/modules/com/mysql/main
+      mkdir -p "$jdbcDriverModuleDirectory"
 
-# Create module for JDBC driver
-jdbcDriverModule=module.xml
-cat <<EOF >${jdbcDriverModule}
+      # Download JDBC driver and passwordless extensions
+      extensionJarName=azure-identity-extensions-1.1.20.jar
+      extensionPomName=azure-identity-extensions-1.1.20.pom
+      sudo curl --retry 5 -Lo ${jdbcDriverModuleDirectory}/${extensionJarName} https://repo1.maven.org/maven2/com/azure/azure-identity-extensions/1.1.20/$extensionJarName
+      sudo curl --retry 5 -Lo ${jdbcDriverModuleDirectory}/${extensionPomName} https://repo1.maven.org/maven2/com/azure/azure-identity-extensions/1.1.20/$extensionPomName
+
+      sudo yum install maven -y
+      sudo mvn dependency:copy-dependencies  -f ${jdbcDriverModuleDirectory}/${extensionPomName} -Ddest=${jdbcDriverModuleDirectory}
+
+      # Create module for JDBC driver
+      jdbcDriverModule=module.xml
+      sudo cat <<EOF >${jdbcDriverModule}
+  <?xml version="1.0" ?>
+  <module xmlns="urn:jboss:module:1.1" name="com.mysql">
+    <resources>
+      <resource-root path="${extensionJarName}"/>
+  EOF
+
+      # Add all jars from target/dependency
+      for jar in ${jdbcDriverModuleDirectory}/target/dependency/*.jar; do
+      if [ -f "$jar" ]; then
+      # Extract just the filename from the path
+      jarname=$(basename "$jar")
+      echo "    <resource-root path=\"target/dependency/${jarname}\"/>" >> ${jdbcDriverModule}
+      fi
+      done
+
+      # Add the closing tags
+      cat <<EOF >> ${jdbcDriverModule}
+    </resources>
+    <dependencies>
+      <module name="javaee.api"/>
+      <module name="sun.jdk"/>
+      <module name="ibm.jdk"/>
+      <module name="javax.api"/>
+      <module name="javax.transaction.api"/>
+    </dependencies>
+  </module>
+  EOF
+
+      chmod 644 $jdbcDriverModule
+      mv $jdbcDriverModule $jdbcDriverModuleDirectory/$jdbcDriverModule
+
+      export passwordlessConnectionString="$dsConnectionString?sslmode=require&user=$uamiDisplayname&authenticationPluginClassName=com.azure.identity.extensions.jdbc.postgresql.AzurePostgresqlAuthenticationPlugin"
+
+      # Register JDBC driver
+        sudo -u jboss $eapRootPath/bin/jboss-cli.sh --connect --echo-command \
+        "/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-xa-datasource-class-name=com.mysql.cj.jdbc.MysqlXADataSource, driver-class-name=com.mysql.cj.jdbc.Driver)" | log
+
+        # Create data source
+        echo "data-source add --driver-name=mysql --name=${jdbcDataSourceName} --jndi-name=${jdbcDSJNDIName} --connection-url=${dsConnectionString} --user-name=${databaseUser} --password=*** --validate-on-match=true --background-validation=false --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLExceptionSorter" | log
+        sudo -u jboss $eapRootPath/bin/jboss-cli.sh --connect --echo-command \
+        "data-source add --driver-name=mysql --name=${jdbcDataSourceName} --jndi-name=${jdbcDSJNDIName} --connection-url=${dsConnectionString} --user-name=${databaseUser} --password=${databasePassword} --validate-on-match=true --background-validation=false --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLExceptionSorter"
+
+else
+  # Create JDBC driver and module directory
+  jdbcDriverModuleDirectory="$eapRootPath"/modules/com/mysql/main
+  mkdir -p "$jdbcDriverModuleDirectory"
+
+  # Download JDBC driver
+  jdbcDriverVersion=8.0.32
+  jdbcDriverName=mysql-connector-j-${jdbcDriverVersion}.jar
+  curl --retry 5 -Lo ${jdbcDriverModuleDirectory}/${jdbcDriverName} https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/${jdbcDriverVersion}/${jdbcDriverName}
+
+  # Create module for JDBC driver
+  jdbcDriverModule=module.xml
+  cat <<EOF >${jdbcDriverModule}
 <?xml version="1.0" ?>
 <module xmlns="urn:jboss:module:1.1" name="com.mysql">
   <resources>
@@ -41,14 +105,21 @@ cat <<EOF >${jdbcDriverModule}
   </dependencies>
 </module>
 EOF
-chmod 644 $jdbcDriverModule
-mv $jdbcDriverModule $jdbcDriverModuleDirectory/$jdbcDriverModule
 
-# Register JDBC driver
-sudo -u jboss $eapRootPath/bin/jboss-cli.sh --connect --echo-command \
-"/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-xa-datasource-class-name=com.mysql.cj.jdbc.MysqlXADataSource, driver-class-name=com.mysql.cj.jdbc.Driver)" | log
+  chmod 644 $jdbcDriverModule
+  mv $jdbcDriverModule $jdbcDriverModuleDirectory/$jdbcDriverModule
 
-# Create data source
-echo "data-source add --driver-name=mysql --name=${jdbcDataSourceName} --jndi-name=${jdbcDSJNDIName} --connection-url=${dsConnectionString} --user-name=${databaseUser} --password=*** --validate-on-match=true --background-validation=false --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLExceptionSorter" | log
-sudo -u jboss $eapRootPath/bin/jboss-cli.sh --connect --echo-command \
-"data-source add --driver-name=mysql --name=${jdbcDataSourceName} --jndi-name=${jdbcDSJNDIName} --connection-url=${dsConnectionString} --user-name=${databaseUser} --password=${databasePassword} --validate-on-match=true --background-validation=false --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLExceptionSorter"
+  # Register JDBC driver
+  sudo -u jboss $eapRootPath/bin/jboss-cli.sh --connect --echo-command \
+  "/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-xa-datasource-class-name=com.mysql.cj.jdbc.MysqlXADataSource, driver-class-name=com.mysql.cj.jdbc.Driver)" | log
+
+  # Create data source
+  echo "data-source add --driver-name=mysql --name=${jdbcDataSourceName} --jndi-name=${jdbcDSJNDIName} --connection-url=${dsConnectionString} --user-name=${databaseUser} --password=*** --validate-on-match=true --background-validation=false --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLExceptionSorter" | log
+  sudo -u jboss $eapRootPath/bin/jboss-cli.sh --connect --echo-command \
+  "data-source add --driver-name=mysql --name=${jdbcDataSourceName} --jndi-name=${jdbcDSJNDIName} --connection-url=${dsConnectionString} --user-name=${databaseUser} --password=${databasePassword} --validate-on-match=true --background-validation=false --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLExceptionSorter"
+
+fi
+
+
+
+
