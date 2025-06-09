fb4f326d1bf624288aa9e2a3ad877a3aad825687
zhihaoguo <zhihaoguo@microsoft.com>
2025-06-09 21:29:45 +0800

    Update retryInterval in test-url-health-check.yaml and enhance logging in url-health-check.sh

--- .github/actions/url-health-check/url-health-check.sh
+++ .github/actions/url-health-check/url-health-check.sh
@@ -9,16 +9,16 @@ while [ $attempt -le $max_retries ]; do
   echo "Response code: $response, Attempt: $attempt"
 
   if [ "$response" -eq 200 ]; then
-    echo "appEndpoint is accessible"
+    echo "appEndpoint ${APP_ENDPOINT} is accessible"
     echo "status=success" >> "$GITHUB_OUTPUT"
     exit 0
   fi
 
-  echo "appEndpoint is not accessible. Retrying in $retry_interval seconds..."
+  echo "appEndpoint ${APP_ENDPOINT} is not accessible. Retrying in $retry_interval seconds..."
   attempt=$((attempt + 1))
   sleep $retry_interval
 done
 
-echo "appEndpoint is not accessible after $max_retries attempts"
+echo "appEndpoint ${APP_ENDPOINT} is not accessible after $max_retries attempts"
 echo "status=failure" >> "$GITHUB_OUTPUT"
 exit 1
